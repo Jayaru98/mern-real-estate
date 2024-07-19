7c58eb1957910a1e4cabd06a5edb987078225f2a
Jayaru Bimsara <jayarubimsaraj@gmail.com>
2024-07-19 15:39:39 +0530

    create get user listings api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,52 +1,52 @@
 import Listing from "../models/listing.model.js";
 
-export const createListingHandler = async (req, res, next) => {
-  const {
-    name,
-    description,
-    address,
-    regularPrice,
-    discountPrice,
-    bathrooms,
-    bedrooms,
-    furnished,
-    parking,
-    type,
-    offer,
-    imageUrls,
-    userRef,
-  
-  } = req.body;
-
-  try {
-    const listing = Listing.create({
-      name,
-      description,
-      address,
-      regularPrice,
-      discountPrice,
-      bathrooms,
-      bedrooms,
-      furnished,
-      parking,
-      type,
-      offer,
-      imageUrls,
-      userRef,
-    
-    });
-
-    res.status(201).json(listing);
-  } catch (error) {
-    next(error);
-  }
-};
-
 // export const createListingHandler = async (req, res, next) => {
+//   const {
+//     name,
+//     description,
+//     address,
+//     regularPrice,
+//     discountPrice,
+//     bathrooms,
+//     bedrooms,
+//     furnished,
+//     parking,
+//     type,
+//     offer,
+//     imageUrls,
+//     userRef,
+  
+//   } = req.body;
+
 //   try {
-//     const listing  = await Listing.create(req.body);
-//     return res.status(201).json(listing);
+//     const listing = Listing.create({
+//       name,
+//       description,
+//       address,
+//       regularPrice,
+//       discountPrice,
+//       bathrooms,
+//       bedrooms,
+//       furnished,
+//       parking,
+//       type,
+//       offer,
+//       imageUrls,
+//       userRef,
+    
+//     });
+
+//     res.status(201).json(listing);
 //   } catch (error) {
-//     next (error);
+//     next(error);
 //   }
-//   };
+// };
+
+export const createListingHandler = async (req, res, next) => {
+  try {
+    const listing  = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next (error);
+  }
+  };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteUser, test, updatedUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updatedUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();   
 router.get('/test',test);
 router.post('/update/:id', verifyToken, updatedUser) 
 router.delete('/delete/:id', verifyToken, deleteUser) 
+router.get('/listings/:id', verifyToken,getUserListings)
 
 export default router;
